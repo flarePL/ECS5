899678ba5c632ac11d3bc1cdbaf752ff859db579
Wojtek FLARE <wojtek@flare.pl>
2021-11-12 14:34:14 +0100

    minor changes

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("12");
+        Debug.Log("1234");
     }
 
     // Update is called once per frame
