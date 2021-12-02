b759da58cce4093493f437676155ee85a3bbb581
Wojtek FLARE <wojtek@flare.pl>
2021-12-02 20:10:23 +0100

    drobne zmiany

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -7,7 +7,7 @@ namespace PionGames.Managers
     {
         [SerializeField] private GameObject asteroidaPrefab;
         [SerializeField] private GameObject statekPrefab;
-        private int grid = 50;
+        private int grid = 150;
 
         public AsteroidsManager asteroidsManager { get; set; }
 
